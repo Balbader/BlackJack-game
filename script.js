12d1f5ca057d8eed25c72a49397c72c76fd48892
Balbader <15971200+Balbader@users.noreply.github.com>
2022-01-14 18:50:11 +0100

    added player object + changes to code in relation to the object created

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ let sumEl = document.getElementById("sum-el");
 let messageEl = document.getElementById("message-el");
 let card = 0
 
+let player = {
+    name: "Balou",
+    chips: 150,
+    isAlive: false,
+    hasBlackJack: false
+}
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
 const getRandom = (min, max) => {
 
     let random = Math.floor(Math.random() * (max - min) + min);
@@ -30,17 +40,17 @@ const renderGame = () => {
         message = "Hit?";
     } else if (sum === 21) {
         message = "BlackJack!";
-        hasBlackJack = true;
+        player.hasBlackJack = true;
     } else {
         message = "You Loose!";
-        isAlive = false;
+        player.isAlive = false;
     }
 
     messageEl.textContent = message;
 }
 
 const startGame = () => {
-    isAlive = true;
+    player.isAlive = true;
     firstCard = getRandom(1, 11);
     secondCard = getRandom(1, 11);
     allCards = [firstCard, secondCard];
@@ -50,33 +60,38 @@ const startGame = () => {
 
 const drawNewCard = () => {
 
-    card = getRandom(1, 11);
-    allCards.push(card);
-    cards.textContent = "Cards:" + allCards;
-    sum += card;
-    sumEl.textContent = "Sum: " + sum;
+    if (player.isAlive && player.hasBlackJack === false) {
+        card = getRandom(1, 11);
+        allCards.push(card);
+        cards.textContent = "Cards:" + allCards;
+        sum += card;
+        sumEl.textContent = "Sum: " + sum;
 
-    if (sum <= 20) {
-        message = "Hit?";
-    } else if (sum === 21) {
-        message = "BlackJack!";
-        hasBlackJack = true;
-    } else {
-        message = "You Loose!";
-        isAlive = false;
+        if (sum <= 20) {
+            message = "Hit?";
+        } else if (sum === 21) {
+            message = "BlackJack!";
+            player.hasBlackJack = true;
+        } else {
+            message = "You Loose!";
+            player.isAlive = false;
+        }
+
+        messageEl.textContent = message;
+
     }
-
-    messageEl.textContent = message;
 }
 
 // Start a new game
 const start = document.getElementById("start");
+
 start.addEventListener("click", () => {
     startGame();
 });
 
 // Draw a new card
 const newCard = document.getElementById("new-card");
+
 newCard.addEventListener("click", () => {
     drawNewCard();
 })
