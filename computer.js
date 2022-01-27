2a461eab77f16e0e9cf28ad4bde068aa0985ed87
Balbader <15971200+Balbader@users.noreply.github.com>
2022-01-27 15:11:06 +0100

    adding computer play: work in progress

--- computer.js
+++ computer.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+/*
+ *
+*/
+
+
+// Get random card
+const getRandomCard = (min, max) => {
+	const random = Math.floor((Math.random() * (max - min) + min));
+
+	if (random === 1) {
+		return 11;
+	}
+
+	if (random > 10) {
+		return 10;
+	}
+
+	return random;
+};
