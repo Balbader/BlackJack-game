6100ff0f79876090a8b7188518868d4270d90e39
Balbader <15971200+Balbader@users.noreply.github.com>
2022-02-02 18:56:22 +0100

    update cards object

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const player = {
 	isAlive: false,
 	hasBlackJack: false,
-	cards: []
+	cards: [],
 };
 
 let allCards = [];
@@ -13,9 +13,94 @@ const messageElement = document.querySelector('#message-el');
 const newCard = document.querySelector('#new-card');
 const start = document.querySelector('#start');
 const newGame = document.querySelector('#new-game');
-const playerElement = document.querySelector('#player-el');
 
-playerElement.textContent = player.name + ': $' + player.chips;
+// Img html tag
+const playerCard1 = document.querySelector('#pl-1');
+const playerCard2 = document.querySelector('#pl-2');
+const computerCard1 = document.querySelector('#cp-1');
+const computerCard2 = document.querySelector('#cp-2');
+
+const eleven = {
+	aceOfClubs: './cards/ace_of_clubs.svg',
+	aceOfHearts: './cards/ace_of_hearts.svg',
+	aceOfSpades: './cards/ace_of_spades.svg',
+	aceOfDiamonds: './cards/ace_of_diamonds.svg'
+};
+
+const ten = {
+	kingOfClubs: './cards/king_of_clubs.svg',
+	kingOfHearts: './cards/king_of_hearts.svg',
+	kingOfSpades: './cards/king_of_spades.svg',
+	kingOfDiamonds: './cards/king_of_diamonds.svg',
+	queenOfClubs: './cards/queen_of_clubs.svg',
+	queenOfHearts: './cards/queen_of_hearts.svg',
+	queenOfSpades: './cards/queen_of_spades.svg',
+	queenOfDiamonds: './cards/queen_of_diamonds.svg',
+	jackOfClubs: './cards/jack_of_clubs.svg',
+	jackOfHearts: './cards/jack_of_hearts.svg',
+	jackOfSpades: './cards/jack_of_spades.svg',
+	jackOfDiamonds: './cards/jack_of_diamonds.svg',
+	tenOfClubs: './cards/10_of_clubs.svg',
+	tenOfHearts: './cards/10_of_hearts.svg',
+	tenOfSpades: './cards/10_of_spades.svg',
+	tenOfDiamonds: './cards/10_of_diamonds.svg'
+};
+
+const nine = {
+	nineOfClubs: './cards/9_of_clubs.svg',
+	nineOfHearts: './cards/9_of_hearts.svg',
+	nineOfSpades: './cards/9_of_spades.svg',
+	nineOfDiamonds: './cards/9_of_diamonds.svg',
+};
+
+const eight = {
+	eightOfClubs: './cards/8_of_clubs.svg',
+	eightOfHearts: './cards/8_of_hearts.svg',
+	eightOfSpades: './cards/8_of_spades.svg',
+	eightOfDiamonds: './cards/8_of_diamonds.svg',
+};
+
+const seven = {
+	sevenOfClubs: './cards/7_of_clubs.svg',
+	sevenOfHearts: './cards/7_of_hearts.svg',
+	sevenOfSpades: './cards/7_of_spades.svg',
+	sevenOfDiamonds: './cards/7_of_diamonds.svg',
+};
+
+const six = {
+	sixOfClubs: './cards/6_of_clubs.svg',
+	sixOfHearts: './cards/6_of_hearts.svg',
+	sixOfSpades: './cards/6_of_spades.svg',
+	sixOfDiamonds: './cards/6_of_diamonds.svg',
+};
+
+const five = {
+	fiveOfClubs: './cards/5_of_clubs.svg',
+	fiveOfHearts: './cards/5_of_hearts.svg',
+	fiveOfSpades: './cards/5_of_spades.svg',
+	fiveOfDiamonds: './cards/5_of_diamonds.svg',
+};
+
+const four = {
+	fourOfClubs: './cards/4_of_clubs.svg',
+	fourOfHearts: './cards/4_of_hearts.svg',
+	fourOfSpades: './cards/4_of_spades.svg',
+	fourOfDiamonds: './cards/4_of_diamonds.svg',
+};
+
+const three = {
+	threeOfClubs: './cards/3_of_clubs.svg',
+	threeOfHearts: './cards/3_of_hearts.svg',
+	threeOfSpades: './cards/3_of_spades.svg',
+	threeOfDiamonds: './cards/3_of_diamonds.svg',
+};
+
+const two = {
+	twoOfClubs: './cards/2_of_clubs.svg',
+	twoOfHearts: './cards/2_of_hearts.svg',
+	twoOfSpades: './cards/2_of_spades.svg',
+	twoOfDiamonds: './cards/2_of_diamonds.svg',
+};
 
 // Get random card
 const getRandomCard = (min, max) => {
