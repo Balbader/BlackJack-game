766ec5e53f1a84f304c3ad35a5053239bb5c9e37
Balbader <15971200+Balbader@users.noreply.github.com>
2022-02-22 07:36:52 +0100

    update resetGame, hit & checkPlayerStatus functions

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Create player object
 const player = {
-	isAlive: false,
-	hasBlackJack: false,
-	cards: [],
+  isAlive: false,
+  hasBlackJack: false,
+  cards: [],
 };
 
 let allCards = [];
@@ -22,83 +22,83 @@ const computerCard2 = document.querySelector('#cp-2');
 
 // Get random card
 const getRandomCard = (min, max) => {
-	const random = Math.floor((Math.random() * (max - min)) + min);
+  const random = Math.floor((Math.random() * (max - min)) + min);
 
-	if (random === 1) {
-		return 11;
-	}
+  if (random === 1) {
+    return 11;
+  }
 
-	if (random > 10) {
-		return 10;
-	}
+  if (random > 10) {
+    return 10;
+  }
 
-	return random;
+  return random;
 };
 
 // Start a game
 const startGame = () => {
-	start.disabled = true;
-	newGame.disabled = true;
-	newCard.disabled = false;
-	player.isAlive = true;
+  start.disabled = true;
+  newGame.disabled = true;
+  newCard.disabled = false;
+  player.isAlive = true;
 
-	allCards = [];
+  allCards = [];
 
-	newCard.addEventListener('click', () => {
-		drawNewCard(allCards);
-		console.log(allCards);
-	});
+  newCard.addEventListener('click', () => {
+    drawNewCard(allCards);
+    console.log(allCards);
+  });
 
-	drawNewCard(allCards);
-	drawNewCard(allCards);
-	console.log(allCards);
+  drawNewCard(allCards);
+  drawNewCard(allCards);
+  console.log(allCards);
 };
 
 // Draw a new card for player
 const drawNewCard = array => {
-	if (player.isAlive === true && player.hasBlackJack === false) {
-		const card = getRandomCard(1, 11);
-		array.push(card);
-		cards.textContent = 'Cards: ' + array;
-	}
+  if (player.isAlive === true && player.hasBlackJack === false) {
+    const card = getRandomCard(1, 11);
+    array.push(card);
+    cards.textContent = 'Cards: ' + array;
+  }
 
-	const sum = array.reduce((left, right) => (left + right), 0);
-	sumElement.textContent = 'Player Total: ' + sum;
+  const sum = array.reduce((left, right) => (left + right), 0);
+  sumElement.textContent = 'Player Total: ' + sum;
 
-	checkPlayerStatus(sum);
+  checkPlayerStatus(sum);
 };
 
 // Reset the game
 const resetGame = () => {
-	newGame.disabled = false;
-	newGame.addEventListener('click', () => {
-		startGame();
-	});
+  newGame.disabled = false;
+  newGame.addEventListener('click', () => {
+    startGame();
+  });
 };
 
 // Check player status
 const checkPlayerStatus = sum => {
-	let message = '';
+  let message = '';
 
-	if (sum <= 20) {
-		message = 'Hit?';
-	} else if (sum === 21) {
-		message = 'BlackJack!';
-		player.hasBlackJack = true;
-		newCard.disabled = true;
-		resetGame();
-	} else {
-		message = 'You Loose!';
-		player.isAlive = false;
-		newCard.disabled = true;
-		resetGame();
-	}
+  if (sum <= 20) {
+    message = 'Hit?';
+  } else if (sum === 21) {
+    message = 'BlackJack!';
+    player.hasBlackJack = true;
+    newCard.disabled = true;
+    resetGame();
+  } else {
+    message = 'You Loose!';
+    player.isAlive = false;
+    newCard.disabled = true;
+    resetGame();
+  }
 
-	messageElement.textContent = message;
+  messageElement.textContent = message;
 };
 
 // Start a new game
 start.addEventListener('click', () => {
-	startGame();
+  startGame();
 });
 
